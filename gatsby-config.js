3c08d0c866de74ce5cc4683365885d7fbb5c9b2a
Ryan Sperzel <ryansperzel@gmail.com>
2020-02-14 18:39:28 -0500

    Added Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan Sperzel | Developer`,
+    description: `This is my test description`,
+    author: `Ryan Sperzel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
